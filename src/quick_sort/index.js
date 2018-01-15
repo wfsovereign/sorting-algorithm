af4635f2aa22134408196a719fc5c3ab60678e56
wfsovereign <wfsovereign@outlook.com>
2018-01-15 17:21:43 +0800

    add lomuto quick sort method and test case

---
+++ src/quick_sort/index.js
@@ -0,0 +1,34 @@
+export function quickSort(array, left, right) {
+  if (left < right) {
+    const partitionIndex = partition(array, left, right);
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+
+  return array;
+}
+
+function partition(array, left, right) {
+  const pivotValue = array[right];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+
+  if (pivotValue < array[partitionIndex]) {
+    swap(array, right, partitionIndex);
+  }
+
+  return partitionIndex;
+}
+
+
+function swap(array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
---
+++ test/quick_sort/index.js
@@ -0,0 +1,31 @@
+import 'should';
+import { quickSort } from '../../src/quick_sort';
+
+
+describe('quick sort test', () => {
+
+  it('should return [1, 2, 3, 4, 6] when access [1, 3, 2, 4, 6]', () => {
+    const result = quickSort([1, 3, 2, 4, 6], 0, 4);
+
+    result.should.be.Array();
+    result.should.deepEqual([1, 2, 3, 4, 6]);
+  });
+
+  it('should return [2, 3, 4, 5, 7, 9] when access [7, 5, 2, 4, 3, 9]', () => {
+    const result = quickSort([7, 5, 2, 4, 3, 9], 0, 5);
+
+    result.should.deepEqual([2, 3, 4, 5, 7, 9]);
+  });
+
+  it('should return [1, 2, 3, 4, 5, 6] when access [1, 2, 3, 4, 5, 6]', () => {
+    const result = quickSort([1, 2, 3, 4, 5, 6], 0, 5);
+
+    result.should.deepEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  it('should return [9, 7, 5, 4, 3, 1] when access [1, 3, 4, 5, 7, 9]', () => {
+    const result = quickSort([9, 7, 5, 4, 3, 1], 0, 5);
+
+    result.should.deepEqual([1, 3, 4, 5, 7, 9]);
+  });
+});
